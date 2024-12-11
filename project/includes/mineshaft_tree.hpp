50a2871f3edbffadb7d2c3cea159c2f57c6dfa8e
ryan-kumar <rdkumar2@illinois.edu>
2024-12-11 03:23:20 +0000

    boilerplate code up

---
+++ project/includes/mineshaft_tree.hpp
@@ -0,0 +1,6 @@
+#ifndef MINESHAFT_TREE_HPP
+#define MINESHAFT_TREE_HPP
+#include <string>
+#include <vector>
+
+#endif
---
+++ project/tests/tests.cc
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "catch.hpp"
+#include "mineshaft_tree.hpp"
+
+TEST_CASE("Student test", "[test]") { REQUIRE(true); }
